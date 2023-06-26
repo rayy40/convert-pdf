3bfe4492b5c444bedf4267a74d59e17ee5f6775e
Rayyan Alam <rayyanalam028@gmail.com>
2023-06-26 12:30:47 +0530

    Added some context states

--- client/src/Helper/FileContext.js
+++ client/src/Helper/FileContext.js
@@ -1,45 +1,69 @@
 import { createContext, useState } from "react";
 
 export const FileContext = createContext({
-  uploadUrl: "",
-  setUploadUrl: () => {},
+  user: null,
+  setUser: () => {},
+  hasAccount: false,
+  setHasAccount: () => {},
   metadata: null,
   setMetadata: () => {},
   showInput: false,
   setShowInput: () => {},
-  isUploading: false,
-  setIsUploading: () => {},
+  isModalOpen: false,
+  setIsModalOpen: () => {},
   isConverting: false,
   setIsConverting: () => {},
   isModifying: false,
-  setIsModifying: () => {},
+  uploadUrl: "",
+  setUploadUrl: () => {},
+  fileDetails: [],
+  setFileDetails: () => {},
+  isRenaming: false,
+  setIsRenaming: () => {},
+  isError: false,
+  setIsError: () => {},
   isCheckboxSelected: false,
   setIsCheckboxSelected: () => {},
 });
 
 export const FileProvider = (props) => {
-  const [uploadUrl, setUploadUrl] = useState("");
+  const [user, setUser] = useState(null);
+  const [hasAccount, setHasAccount] = useState(false);
   const [metadata, setMetadata] = useState(null);
-  const [isUploading, setIsUploading] = useState(false);
-  const [isModifying, setIsModifying] = useState(false);
-  const [isConverting, setIsConverting] = useState(false);
+  const [fileDetails, setFileDetails] = useState([]);
   const [showInput, setShowInput] = useState(false);
+  const [uploadUrl, setUploadUrl] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isConverting, setIsConverting] = useState(false);
+  const [isRenaming, setIsRenaming] = useState(false);
+  const [isModifying, setIsModifying] = useState(false);
   const [isCheckboxSelected, setIsCheckboxSelected] = useState(null);
   return (
     <FileContext.Provider
       value={{
-        uploadUrl,
-        setUploadUrl,
-        showInput,
-        setShowInput,
+        user,
+        setUser,
+        hasAccount,
+        setHasAccount,
         metadata,
         setMetadata,
-        isUploading,
-        setIsUploading,
+        fileDetails,
+        setFileDetails,
+        showInput,
+        setShowInput,
+        uploadUrl,
+        setUploadUrl,
+        isError,
+        setIsError,
+        isModalOpen,
+        setIsModalOpen,
         isConverting,
         setIsConverting,
         isModifying,
         setIsModifying,
+        isRenaming,
+        setIsRenaming,
         isCheckboxSelected,
         setIsCheckboxSelected,
       }}
