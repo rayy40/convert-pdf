3ac575dc5e4b1dbbe5751020c6c81fc883d46db6
Rayyan Alam <rayyanalam028@gmail.com>
2023-06-26 13:05:32 +0530

    Changed

--- client/src/Pages/OperationsPages/ResultPage.jsx
+++ client/src/Pages/OperationsPages/ResultPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {
   faArrowRotateRight,
   faDownload,
-  faPen,
+  // faPen,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, useLocation } from "react-router-dom";
