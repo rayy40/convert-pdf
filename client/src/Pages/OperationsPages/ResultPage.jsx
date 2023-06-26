36d5f8ac40df090ebf3db5846fd35a933f2b4a1c
Rayyan Alam <rayyanalam028@gmail.com>
2023-06-26 12:31:33 +0530

    Added a txt file placeholder

--- client/src/Pages/OperationsPages/ResultPage.jsx
+++ client/src/Pages/OperationsPages/ResultPage.jsx
@@ -1,14 +1,16 @@
+import React from "react";
 import {
   faArrowRotateRight,
   faDownload,
+  faPen,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import pdf from "../../Assets/pdf-placeholder.png";
 import jpg from "../../Assets/jpg-placeholder.png";
 import ppt from "../../Assets/ppt-placeholder.png";
 import zip from "../../Assets/zip-placeholder.png";
+import txt from "../../Assets/txt-placeholder.png";
 import docx from "../../Assets/word-placeholder.png";
 import protectPdf from "../../Assets/protected-pdf-placeholder.png";
 
@@ -23,11 +25,12 @@ const ResultPage = () => {
       return pdf;
     } else if (extension === "pptx" || extension === "ppt") return ppt;
     else if (extension === "jpg") return jpg;
+    else if (extension === "txt") return txt;
     else if (extension === "zip") return zip;
     else if (extension === "docx") return docx;
   };
 
-  const decodeUrl = decodeURIComponent(location.state);
+  const decodeUrl = decodeURIComponent(location?.state?.url);
   const filename = decodeUrl.split("/").pop().split("?")[0];
 
   return (
@@ -36,13 +39,14 @@ const ResultPage = () => {
         <div className="document-container">
           <Link
             target="_blank"
-            to={location?.state.replace(/"/g, "")}
+            to={location?.state?.url?.replace(/"/g, "")}
+            rel="noopener noreferrer"
             download
             className="link"
           >
             <img
               src={renderImg(
-                filename?.substring(filename.lastIndexOf(".") + 1)
+                filename?.substring(filename?.lastIndexOf(".") + 1)
               )}
               alt="doc-img"
             />
@@ -50,21 +54,27 @@ const ResultPage = () => {
         </div>
         <div className="details-container">
           <div className="filename">
-            <h3>{filename?.substring(0, filename.lastIndexOf("."))}</h3>
-            <span>{filename?.substring(filename.lastIndexOf("."))}</span>
+            <h3>{filename?.substring(0, filename?.lastIndexOf("."))}</h3>
+            <span>{filename?.substring(filename?.lastIndexOf("."))}</span>
           </div>
 
           <Link
+            target="_blank"
             download
+            rel="noopener noreferrer"
             className="link"
-            to={location?.state.replace(/"/g, "")}
+            to={location?.state?.url?.replace(/"/g, "")}
           >
             <button className="download-btn">
               <FontAwesomeIcon className="icon" icon={faDownload} />
               Download
             </button>
           </Link>
-          <Link className="start-over-link" to={"/"}>
+          {/* <button className="rename-btn">
+            Rename
+            <FontAwesomeIcon className="icon" icon={faPen} />
+          </button> */}
+          <Link className="start-over-link" to={`/${location?.state?.route}`}>
             <FontAwesomeIcon className="icon" icon={faArrowRotateRight} /> Start
             Over
           </Link>
